39a755c00db1ad8510971fe4b6bb489524184e63
jpchavescruz <j.pvplayer0@gmail.com>
2024-04-17 16:22:09 -0300

    fix: fix edit script not loading

--- src/js/edit_script.js
+++ src/js/edit_script.js
@@ -1,6 +1,8 @@
-const item = data[searchParams.get('key')]
-bookName.value = item.bookName
-autorName.value = item.autorName
-category.value = item.category
-publiYear.value = item.publiYear
-price.value = parseFloat(item.price).toFixed(2).replace('.', ',')
+document.addEventListener('DOMContentLoaded', () => {
+  const item = data[searchParams.get('key')]
+  bookName.value = item.bookName
+  autorName.value = item.autorName
+  category.value = item.category
+  publiYear.value = item.publiYear
+  price.value = parseFloat(item.price).toFixed(2).replace('.', ',')
+})
